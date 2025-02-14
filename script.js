7eaa23b30b5f0dff0d079336629808763048e77c
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-14 13:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-    const selectElement = document.getElementById("colorSelect");
-    const button = document.querySelector("input[type='button']");
-    
-    button.addEventListener("click", function () {
-        const selectedIndex = selectElement.selectedIndex;
-        if (selectedIndex !== -1) {
-            selectElement.remove(selectedIndex);
-        }
-    });
-});
+const colorSelect = document.getElementById("colorSelect");
+const removeButton = document.querySelector("input[type='button']");
+
+removeButton.onclick = function() {
+    colorSelect.remove(colorSelect.selectedIndex);
+};
